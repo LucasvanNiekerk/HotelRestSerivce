bccfa1cdcb14d11a47682daa303d6d188baba50e
LucasvanNiekerk <luca1921@edu.easj.dk>
2019-03-11 12:30:13 +0100

    Minor change in name

--- HotelRestService/DBUtil/Utils.cs
+++ HotelRestService/DBUtil/Utils.cs
@@ -4,11 +4,11 @@ namespace HotelRestService.DBUtil
 {
     public class Utils
     {
-        private const string connectionString = "Data Source=luca1921server.database.windows.net;Initial Catalog=Luca1921;User ID=luca1921;Password=password1921!;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private const string ConnectionString = "Data Source=luca1921server.database.windows.net;Initial Catalog=Luca1921;User ID=luca1921;Password=password1921!;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         private static SqlConnection _sqlConnection;
         public static SqlConnection GetConnection()
         {
-            return _sqlConnection ?? (_sqlConnection = new SqlConnection(connectionString));
+            return _sqlConnection ?? (_sqlConnection = new SqlConnection(ConnectionString));
         }
     }
 }
